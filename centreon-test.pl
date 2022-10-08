5ba3bd4e0d8421829727c5366baf7f9ab85e96c9
DSteph <delvalstephan@gmail.com>
2022-10-08 15:34:16 +0200

    Algo ok json to do

--- centreon-test.pl
+++ centreon-test.pl
@@ -17,25 +17,22 @@ while( defined( $l = <$fd> ) ) {
    # Counting chars and words, reading the very first char to get the loop going smooth
    $firstChar = substr($l, 0, 1);
    if ( grep( /^$firstChar$/, @charSeparators ) || $firstChar eq " " ){
-      $isWord = 0; # Will be true if last char was part of a word, cd not a delimiter
+      $isWord = 0; # Will be true if last char was part of a word, cf not a delimiter
    }
    else {
       $isWord = 1;
    }
-
    # We consider the blank not to be counted as a character
    foreach $byte (split //, $l) {
-      switch ( $byte ) {
-         # Simplest to more complex to save processing time
-         case ( $isWord )                                                                          { ++$chars }
-         case ( $byte = " " && $isWord )                                                           { ++$words; $isWord=0 }
-         case ( grep( /^$byte$/, @charSeparators ) && $isWord )                                    { ++$chars; ++$words; $isWord=0 }
-         case ( ( !( grep( /^$byte$/, @charSeparators ) ) || !( $byte eq " " ) ) && !$isWord )     { ++$chars; $isWord=1 }
 
-      }
+      if ( ( $byte eq " " ) && $isWord )                                                         { ++$words; $isWord=0; }
+      elsif ( grep( /^$byte$/, @charSeparators ) && $isWord )                                    { ++$chars; ++$words; $isWord=0 }
+      elsif ( ( !( grep( /^$byte$/, @charSeparators ) ) || !( $byte eq " " ) ) && !$isWord )     { ++$chars; $isWord=1 }
+      elsif ( $isWord )                                                                          { ++$chars }
+
    }
-   
-   print "lines : $lines\n";
-   print "words : $words\n";
-   print "chars : $chars\n";
 }
+
+%output = ( 'Lines' => $lines, 'Words' => $words, 'Characters' => $chars );
+
+print(encode_json(%output));
