2b1bb4bf3595cd2fe85c765b2e07142b1a15fbb6
DSteph <delvalstephan@gmail.com>
2022-10-09 12:51:51 +0200

    Parsing and tests functionnal, added readme description

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # centreontest
+
+The application takes a single argument in the form of an absolute path to the file to be parsed.
+i.e: >perl centreon-test.pl C:/Users/JaneDoe/Desktop/filetoparse.txt
+
+The result will be saved in the same directory as the file to parse as : filetoparse_parsed.json
+
+Multiple tests are run through to make sure the file exists, can be opened/read and that we have the rights to create a new file.
+
+As for now, I considered whistespaces and it's cousins (tab, carriage return etc...) to not be counted as characters, but if need be it would represent a minor change in the code.
--- centreon-test.pl
+++ centreon-test.pl
@@ -1,40 +1,45 @@
 use JSON;
+use Try::Tiny;
 
 # My hope is that this saves processing time, but it probably doesn't
 $path = $ARGV[0];
 
 if( ! (-e $path) ) {
-   print "The file at the specified path $path does not exist\n";
-   exit(1);
+   die("Error: The file at the specified path $path does not exist\n");
 }
 
-open($fd, '<', $ARGV[0]) or die("Could not open the file at specified path $path\n");
+open($fd, '<', $ARGV[0]) or die("Error: Could not open the file at specified path $path\n");
 
 @charSeparators = ( ',', '.' ); 
 $lines, $words, $chars = 0;
-# Reading and counting each line using chomp
-while( defined( $l = <$fd> ) ) {
-   chomp $l;
-   ++$lines;
-   # Counting chars and words, reading the very first char to get the loop going smooth
-   $firstChar = substr($l, 0, 1);
-   if ( grep( /^$firstChar$/, @charSeparators ) || $firstChar eq " " ){
-      $isWord = 0; # Will be true if last char was part of a word, cf not a delimiter
-   }
-   else {
-      $isWord = 1;
-   }
-   # We consider the blank not to be counted as a character
-   foreach $byte (split //, $l) {
 
-      if ( ( $byte eq " " ) && $isWord )                                                         { ++$words; $isWord=0; }
-      elsif ( grep( /^$byte$/, @charSeparators ) && $isWord )                                    { ++$chars; ++$words; $isWord=0 }
-      elsif ( ( !( grep( /^$byte$/, @charSeparators ) ) || !( $byte eq " " ) ) && !$isWord )     { ++$chars; $isWord=1 }
-      elsif ( $isWord )                                                                          { ++$chars }
+# Rather than having a liste of readable file format, we inform the user if we cannot read and let them check format
+try {
+   # Reading and counting each line using chomp
+   while( defined( $l = <$fd> ) ) {
+      chomp $l;
+      ++$lines;
+      # Counting chars and words, reading the very first char to get the loop going smooth
+      $firstChar = substr($l, 0, 1);
+      if ( grep( /^$firstChar$/, @charSeparators ) || $firstChar eq " " ){
+         $isWord = 0; # Will be true if last char was part of a word, cf not a delimiter
+      }
+      else {
+         $isWord = 1;
+      }
+      # We consider the blank not to be counted as a character
+      foreach $byte (split //, $l) {
+         if ( ( $byte =~ /\s/ ) && $isWord )                                                        { ++$words; $isWord=0 }
+         elsif ( grep( /^$byte$/, @charSeparators ) && $isWord )                                    { ++$chars; ++$words; $isWord=0 }
+         elsif ( grep( /^$byte$/, @charSeparators ) && !$isWord )                                   { ++$chars } 
+         elsif ( ( !( grep( /^$byte$/, @charSeparators ) ) && !( $byte eq " " ) ) && !$isWord )     { ++$chars; $isWord=1 }
+         elsif ( $isWord )                                                                          { ++$chars }
 
+      }
    }
-}
-
+} catch {
+   die("Error: Could not read the file at specified path $path, please make sure that it is a text file.\n")
+};
 # We create the json at the same path as origin file
 $savePath = substr( $path, 0, rindex( $path, "." ) ) . '_parsed.json';
 $name = substr( $path, rindex( $path, "/" ) + 1 );
